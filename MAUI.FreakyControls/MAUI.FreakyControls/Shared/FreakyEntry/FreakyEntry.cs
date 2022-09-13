55d06c9bdae3f77bb943b9c20ce6ae774c461da9
Gulam Ali Hakim <jimtiyg@Gulams-MacBook-Pro.local>
2022-09-13 19:35:45 +0530

    fix: added description to all the control properties

--- MAUI.FreakyControls/MAUI.FreakyControls/Shared/FreakyEntry/FreakyEntry.cs
+++ MAUI.FreakyControls/MAUI.FreakyControls/Shared/FreakyEntry/FreakyEntry.cs
@@ -55,48 +55,72 @@ namespace Maui.FreakyControls
               typeof(FreakyEntry),
               true);
 
+        /// <summary>
+        /// Gets and Sets if your Entry allows Copy Paste. default is true!
+        /// </summary>
         public bool AllowCopyPaste
         {
             get => (bool)GetValue(AllowCopyPasteProperty);
             set => SetValue(AllowCopyPasteProperty, value);
         }
 
+        /// <summary>
+        /// Command parameter for your Image tap command 
+        /// </summary>
         public object ImageCommandParameter
         {
             get => GetValue(ImageCommandParameterProperty);
             set => SetValue(ImageCommandParameterProperty, value);
         }
 
+        /// <summary>
+        /// A command that you can use to bind with your Image that you added to your Entry's ViewPort
+        /// </summary>
         public ICommand ImageCommand
         {
             get => (ICommand)GetValue(ImageCommandProperty);
             set => SetValue(ImageCommandProperty, value);
         }
 
+        /// <summary>
+        /// Padding of the Image that you added to the ViewPort
+        /// </summary>
         public int ImagePadding
         {
             get => (int)GetValue(ImagePaddingProperty);
             set => SetValue(ImagePaddingProperty, value);
         }
 
+        /// <summary>
+        /// Width of the Image in your ViewPort
+        /// </summary>
         public int ImageWidth
         {
             get => (int)GetValue(ImageWidthProperty);
             set => SetValue(ImageWidthProperty, value);
         }
 
+        /// <summary>
+        /// Height of the Image in your ViewPort
+        /// </summary>
         public int ImageHeight
         {
             get => (int)GetValue(ImageHeightProperty);
             set => SetValue(ImageHeightProperty, value);
         }
 
+        /// <summary>
+        /// An ImageSource that you want to add to your ViewPort
+        /// </summary>
         public ImageSource ImageSource
         {
             get => (ImageSource)GetValue(ImageSourceProperty);
             set => SetValue(ImageSourceProperty, value);
         }
 
+        /// <summary>
+        /// Alignment for your Image's ViewPort, By default set to Right.
+        /// </summary>
         public ImageAlignment ImageAlignment
         {
             get => (ImageAlignment)GetValue(ImageAlignmentProperty);
--- MAUI.FreakyControls/Samples/MauiProgram.cs
+++ MAUI.FreakyControls/Samples/MauiProgram.cs
@@ -1,5 +1,7 @@
 ﻿using Maui.FreakyControls.Extensions;
 
+// this is to avoid the following https://docs.microsoft.com/en-us/dotnet/maui/xaml/xamlc
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Samples;
 
 public static class MauiProgram
