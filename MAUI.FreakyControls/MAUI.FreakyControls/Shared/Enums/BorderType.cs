958a31bfab571ffd01bba75a6c24f0084a75879a
Gulam Ali Hakim <jimtiyg@Gulams-MacBook-Pro.local>
2022-09-16 13:33:41 +0530

    fix: enums and properties now have details :D

--- MAUI.FreakyControls/MAUI.FreakyControls/Shared/Enums/BorderType.cs
+++ MAUI.FreakyControls/MAUI.FreakyControls/Shared/Enums/BorderType.cs
@@ -3,8 +3,19 @@ namespace Maui.FreakyControls.Shared.Enums
 {
     public enum BorderType
     {
+        /// <summary>
+        /// Borderless control 
+        /// </summary>
         None,
+
+        /// <summary>
+        /// Rectangular border to your control 
+        /// </summary>
         Full,
+
+        /// <summary>
+        /// Underline to your control
+        /// </summary>
         Underline
     }
 }
