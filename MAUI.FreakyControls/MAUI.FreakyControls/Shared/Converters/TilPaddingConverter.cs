baf466ec54bbccf533906efee1d8531cb403ad79
Gulam Ali Hakim <jimtiyg@Gulams-MacBook-Pro.local>
2022-09-29 16:01:07 +0530

    feat: TextInputLayout now has an outlined option :)

---
+++ MAUI.FreakyControls/MAUI.FreakyControls/Shared/Converters/TilPaddingConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Maui.FreakyControls.Shared.Enums;
+
+namespace Maui.FreakyControls.Shared.Converters
+{
+    public class TilPaddingConverter : BaseOneWayValueConverter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var borderType = (BorderType)value;
+#if ANDROID
+            var emptyThickness = new Thickness(10, 0, 10, 0);
+#endif
+#if IOS
+            var emptyThickness = new Thickness(10);
+
+#endif
+            var fullThickness = new Thickness(10);
+            return borderType == BorderType.None ? emptyThickness : borderType == BorderType.Underline ? emptyThickness : fullThickness;
+        }
+    }
+}
+
