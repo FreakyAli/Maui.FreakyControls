33d324f531f8ebcb1c2381f6432595573816b37c
Gulam Ali Hakim <jimtiyg@Gulams-MacBook-Pro.local>
2022-09-30 07:38:09 +0530

    feat: freaky image added for descriptions

--- MAUI.FreakyControls/MAUI.FreakyControls/Shared/FreakyImage/FreakyImage.cs
+++ MAUI.FreakyControls/MAUI.FreakyControls/Shared/FreakyImage/FreakyImage.cs
@@ -1,20 +1,19 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 
-namespace Maui.FreakyControls.Shared.FreakyImage
-{
-	public class FreakyImage : Image
-	{
-        public event EventHandler ImageLoaded;
+namespace Maui.FreakyControls;
 
-        protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+public class FreakyImage : Image
+{
+    public event EventHandler ImageLoaded;
+
+    protected override void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        base.OnPropertyChanged(propertyName);
+        if (propertyName.Equals(IsLoadingProperty.PropertyName))
         {
-            base.OnPropertyChanged(propertyName);
-            if (propertyName.Equals(IsLoadingProperty.PropertyName))
-            {
-                if (IsLoading && Source != null)
-                    ImageLoaded?.Invoke(this, null);
-            }
+            if (IsLoading && Source != null)
+                ImageLoaded?.Invoke(this, null);
         }
     }
 }
