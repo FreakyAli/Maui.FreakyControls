c6a7db182e9281ac319c6d3f8f01db84bd77abf5
Gulam Ali H <31090457+FreakyAli@users.noreply.github.com>
2022-10-10 20:21:09 +0530

    fix: Circular Image now comes from our control. (#17)
    * fix: Circular Image now inherits from our Image control
    
    * fix: update changes for some controls

---
+++ MAUI.FreakyControls/MAUI.FreakyControls/Shared/FreakyAutoCompleteView/AutoCompleteView.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Reflection;
+using System.Windows.Input;
+using Maui.FreakyControls.Extensions;
+using Grid = Microsoft.Maui.Controls.Grid;
+using StackLayout = Microsoft.Maui.Controls.StackLayout;
+
+namespace Maui.FreakyControls;
+
+public partial class AutoCompleteView : ContentView
+{
+    private const int RowHeight = 60;
+
+    public static readonly BindableProperty TextProperty = BindableProperty.Create(
+        nameof(Text),
+        typeof(string),
+        typeof(AutoCompleteView),
+        defaultBindingMode: BindingMode.TwoWay);
+
+    public static readonly BindableProperty TextColorProperty = BindableProperty.Create(
+        nameof(TextColor),
+        typeof(Color),
+        typeof(AutoCompleteView),
+        Colors.Black);
+
+    public static readonly BindableProperty MaximumVisibleElementsProperty = BindableProperty.Create(
+        nameof(MaximumVisibleElements),
+        typeof(int),
+        typeof(AutoCompleteView),
+        4);
+
+    public static readonly BindableProperty MinimumPrefixCharacterProperty = BindableProperty.Create(
+        nameof(MinimumPrefixCharacter),
+        typeof(int),
+        typeof(AutoCompleteView),
+        1);
+
+    public static readonly BindableProperty PlaceholderProperty = BindableProperty.Create(
+        nameof(Placeholder),
+        typeof(string),
+        typeof(AutoCompleteView),
+        string.Empty);
+
+    public static readonly BindableProperty PlaceholderColorProperty = BindableProperty.Create(
+        nameof(PlaceholderColor),
+        typeof(Color),
+        typeof(AutoCompleteView),
+        Colors.DarkGray);
+
+    public static readonly BindableProperty ItemsSourceProperty =BindableProperty.Create(
+        nameof(ItemsSource),
+        typeof(IEnumerable),
+        typeof(AutoCompleteView),
+        null);
+
+
+    private readonly IEnumerable _originSuggestions = Array.Empty<object>();
+
+    public AutoCompleteView()
+    {
+        InitializeComponent();
+    }
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public Color TextColor
+    {
+        get => (Color)GetValue(TextColorProperty);
+        set => SetValue(TextColorProperty, value);
+    }
+
+    public int MaximumVisibleElements
+    {
+        get => (int)GetValue(MaximumVisibleElementsProperty);
+        set => SetValue(MaximumVisibleElementsProperty, value);
+    }
+
+    public int MinimumPrefixCharacter
+    {
+        get => (int)GetValue(MinimumPrefixCharacterProperty);
+        set => SetValue(MinimumPrefixCharacterProperty, value);
+    }
+
+    public IEnumerable ItemsSource
+    {
+        get => (IEnumerable)GetValue(ItemsSourceProperty);
+        set => SetValue(ItemsSourceProperty, value);
+    }
+
+    public string Placeholder
+    {
+        get => (string)GetValue(PlaceholderProperty);
+        set => SetValue(PlaceholderProperty, value);
+    }
+
+    public Color PlaceholderColor
+    {
+        get => (Color)GetValue(PlaceholderColorProperty);
+        set => SetValue(PlaceholderColorProperty, value);
+    }
+}
--- MAUI.FreakyControls/Samples/InputViews/InputViewModel.cs
+++ MAUI.FreakyControls/Samples/InputViews/InputViewModel.cs
@@ -1,9 +1,51 @@
 ﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Nager.Country;
+using static System.Net.Mime.MediaTypeNames;
+using Maui.FreakyControls;
+
 namespace Samples.InputViews
 {
     public class InputViewModel : MainViewModel
     {
+        private string _searchCountry = string.Empty;
+        private bool _customSearchFunctionSwitchIsToggled;
 
+        public string SearchCountry
+        {
+            get => _searchCountry;
+            set
+            {
+                _searchCountry = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public List<string> Countries { get; } 
+
+
+        public InputViewModel()
+        {
+
+            var countryProvider = new CountryProvider();
+            var countries = countryProvider.GetCountries().Select(x=>x.OfficialName);
+            Countries = new List<string>(countries);
+        }
+
+
+        public bool CustomSearchFunctionSwitchIsToggled
+        {
+            get => _customSearchFunctionSwitchIsToggled;
+            set
+            {
+                _customSearchFunctionSwitchIsToggled = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
 
