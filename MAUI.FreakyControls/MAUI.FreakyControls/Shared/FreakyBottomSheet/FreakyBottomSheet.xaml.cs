b033dab4d04f3d3c62c37b5e524ae05b5c2b6a28
FreakyAli <31090457+FreakyAli@users.noreply.github.com>
2022-10-31 01:53:59 +0530

    feat: FreakyBottomSheet is ready for battle

--- MAUI.FreakyControls/MAUI.FreakyControls/Shared/FreakyBottomSheet/FreakyBottomSheet.xaml.cs
+++ MAUI.FreakyControls/MAUI.FreakyControls/Shared/FreakyBottomSheet/FreakyBottomSheet.xaml.cs
@@ -1,28 +1,41 @@
-﻿namespace Maui.FreakyControls;
+﻿using System.Windows.Input;
+using Maui.FreakyControls.Extensions;
+namespace Maui.FreakyControls;
 
 public partial class FreakyBottomSheet : ContentView
 {
     private const string defaultClearIcon = "Maui.FreakyControls.clear_icon.svg";
+    private const uint shortDuration = 250u;
+    private const uint regularDuration = shortDuration * 2u;
+    private View bottomSheetContent;
 
     public FreakyBottomSheet()
     {
         InitializeComponent();
+        this.InputTransparent = true;
+        this.VerticalOptions = LayoutOptions.Fill;
+        this.HorizontalOptions = LayoutOptions.Fill;
         CloseBottomSheetButton.ResourceId = defaultClearIcon;
         CloseBottomSheetButton.SvgAssembly = typeof(FreakyBottomSheet).Assembly;
     }
 
-    private const uint shortDuration = 250u;
-    private const uint regularDuration = shortDuration * 2u;
+    #region Properties & Bindable Properties
 
-    public IList<Microsoft.Maui.IView> BottomSheetContent => BottomSheetContentGrid.Children;
-
-    #region Bindable Properties
+    public View BottomSheetContent
+    {
+        get => bottomSheetContent;
+        set
+        {
+            bottomSheetContent = value;
+            OnPropertyChanged();
+        }
+    }
 
     public static readonly BindableProperty SheetHeightProperty = BindableProperty.Create(
         nameof(SheetHeight),
         typeof(double),
         typeof(FreakyBottomSheet),
-        360d,
+        300d,
         BindingMode.OneWay,
         validateValue: (_, value) => value != null);
 
@@ -32,44 +45,77 @@ public partial class FreakyBottomSheet : ContentView
         set => SetValue(SheetHeightProperty, value);
     }
 
-    public static readonly BindableProperty HeaderTextProperty = BindableProperty.Create(
-        nameof(HeaderText),
-        typeof(string),
-        typeof(FreakyBottomSheet),
-        string.Empty,
-        BindingMode.OneWay,
-        validateValue: (_, value) => value != null);
+    public static readonly BindableProperty SheetBackgroundColorProperty = BindableProperty.Create(
+    nameof(SheetBackgroundColor),
+    typeof(Color),
+    typeof(FreakyBottomSheet),
+    Colors.White);
 
-    public string HeaderText
+    public Color SheetBackgroundColor
     {
-        get => (string)GetValue(HeaderTextProperty);
-        set => SetValue(HeaderTextProperty, value);
+        get => (Color)GetValue(SheetBackgroundColorProperty);
+        set => SetValue(SheetBackgroundColorProperty, value);
     }
 
-    public static readonly BindableProperty HeaderStyleProperty = BindableProperty.Create(
-        nameof(HeaderStyle),
-        typeof(Style),
-        typeof(FreakyBottomSheet),
-        new Style(typeof(Label))
-        {
-            Setters =
-            {
-                new Setter
-                {
-                    Property = Label.FontSizeProperty,
-                    Value = 24
-                }
-            }
-        },
-        BindingMode.OneWay,
-        validateValue: (_, value) => value != null);
+    public static readonly BindableProperty FadeBackgroundColorProperty = BindableProperty.Create(
+    nameof(FadeBackgroundColor),
+    typeof(Color),
+    typeof(FreakyBottomSheet),
+    Colors.LightGrey);
 
-    public Style HeaderStyle
+    public Color FadeBackgroundColor
     {
-        get => (Style)GetValue(HeaderStyleProperty);
-        set => SetValue(HeaderStyleProperty, value);
+        get => (Color)GetValue(FadeBackgroundColorProperty);
+        set => SetValue(FadeBackgroundColorProperty, value);
     }
 
+    public static readonly BindableProperty CloseIconColorProperty = BindableProperty.Create(
+    nameof(CloseIconColor),
+    typeof(Color),
+    typeof(FreakyBottomSheet),
+    Colors.Black);
+
+    public Color CloseIconColor
+    {
+        get => (Color)GetValue(CloseIconColorProperty);
+        set => SetValue(CloseIconColorProperty, value);
+    }
+
+    public static readonly BindableProperty FadeOpacityProperty = BindableProperty.Create(
+    nameof(FadeOpacity),
+    typeof(double),
+    typeof(FreakyBottomSheet),
+    0d);
+
+    public double FadeOpacity
+    {
+        get => (double)GetValue(FadeOpacityProperty);
+        set => SetValue(FadeOpacityProperty, value);
+    }
+
+    public static readonly BindableProperty CloseCommmandProperty = BindableProperty.Create(
+    nameof(CloseCommmand),
+    typeof(ICommand),
+    typeof(FreakyBottomSheet),
+    default(ICommand));
+
+    public ICommand CloseCommmand
+    {
+        get =>(ICommand) GetValue(CloseCommmandProperty);
+        set => SetValue(CloseCommmandProperty, value);
+    }
+
+    public static readonly BindableProperty CloseCommmandParameterProperty = BindableProperty.Create(
+    nameof(CloseCommmandParameter),
+    typeof(object),
+    typeof(FreakyBottomSheet),
+    default(object));
+
+    public object CloseCommmandParameter
+    {
+        get => (object)GetValue(CloseCommmandParameterProperty);
+        set => SetValue(CloseCommmandParameterProperty, value);
+    }
 
     #endregion
 
@@ -95,6 +141,14 @@ public partial class FreakyBottomSheet : ContentView
         this.InputTransparent = true;
     }
 
-    async void CloseBottomSheetButton_Tapped(System.Object sender, System.EventArgs e) =>
+    async void CloseBottomSheetButton_Tapped(System.Object sender, System.EventArgs e)
+    {
+        CloseCommmand?.ExecuteCommandIfAvailable(CloseCommmandParameter);
         await CloseAsync();
+    }
+
+    async void BackgroundFade_Tapped(System.Object sender, System.EventArgs e)
+    {
+        await CloseAsync();
+    }
 }
