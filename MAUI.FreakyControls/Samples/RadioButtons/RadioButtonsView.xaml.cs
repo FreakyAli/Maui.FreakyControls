f2a0324f87365835b4ab7ce56d0ad42a1746be2c
FreakyAli <31090457+FreakyAli@users.noreply.github.com>
2023-02-25 16:54:05 +0530

    fix: radiobuttons example updated

--- MAUI.FreakyControls/Samples/RadioButtons/RadioButtonsView.xaml.cs
+++ MAUI.FreakyControls/Samples/RadioButtons/RadioButtonsView.xaml.cs
@@ -2,11 +2,11 @@
 
 public partial class RadioButtonsView : ContentPage
 {
-	public RadioButtonsView()
-	{
-		InitializeComponent();
-		BindingContext = new RadioButtonsViewModel();
-	}
+    public RadioButtonsView()
+    {
+        InitializeComponent();
+        BindingContext = new RadioButtonsViewModel();
+    }
 
     void FreakyRadioGroup_SelectedRadioButtonChanged(System.Object sender, Maui.FreakyControls.FreakyRadioButtonEventArgs e)
     {
