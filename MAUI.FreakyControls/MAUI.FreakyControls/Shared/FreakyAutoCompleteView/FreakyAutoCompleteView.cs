bd2e84749f4928340bf83b5f737ec5221c335c22
Gulam Ali H <31090457+FreakyAli@users.noreply.github.com>
2023-10-02 02:06:34 +0530

    fix: some code clean up and more

--- MAUI.FreakyControls/MAUI.FreakyControls/Shared/FreakyAutoCompleteView/FreakyAutoCompleteView.cs
+++ MAUI.FreakyControls/MAUI.FreakyControls/Shared/FreakyAutoCompleteView/FreakyAutoCompleteView.cs
@@ -159,34 +159,6 @@ public class FreakyAutoCompleteView : View, IFreakyAutoCompleteView
           typeof(FreakyAutoCompleteView),
           default(object));
 
-    public static readonly BindableProperty CharacterSpacingProperty = BindableProperty.Create(
-            nameof(CharacterSpacing),
-            typeof(double),
-            typeof(FreakyAutoCompleteView),
-            default(double)
-            );
-
-    public static readonly BindableProperty KeyboardProperty = BindableProperty.Create(
-        nameof(Keyboard),
-        typeof(Microsoft.Maui.Keyboard),
-        typeof(FreakyAutoCompleteView),
-        Keyboard.Default,
-        coerceValue: (o, v) => (Keyboard)v ?? Keyboard.Default
-        );
-
-    [System.ComponentModel.TypeConverter(typeof(Microsoft.Maui.Converters.KeyboardTypeConverter))]
-    public Keyboard Keyboard
-    {
-        get => (Keyboard)GetValue(KeyboardProperty);
-        set => SetValue(KeyboardProperty, value);
-    }
-
-    public double CharacterSpacing
-    {
-        get => (double)GetValue(CharacterSpacingProperty);
-        set => SetValue(CharacterSpacingProperty, value);
-    }
-
     public object ImageCommandParameter
     {
         get => GetValue(ImageCommandParameterProperty);
