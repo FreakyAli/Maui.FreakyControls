1e9c9c93528170c3a07152d0cee14b92a4cf4f6a
Gulam Ali H <31090457+FreakyAli@users.noreply.github.com>
2023-10-01 15:42:28 +0530

    fix: FreakySwitch is here :)

---
+++ MAUI.FreakyControls/MAUI.FreakyControls/Shared/FreakySwitch/FreakySwitch.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Handlers;
+#if ANDROID
+using Android.Content;
+using AndroidSwitch = Google.Android.Material.MaterialSwitch.MaterialSwitch;
+#endif
+namespace Maui.FreakyControls;
+
+public class FreakySwitch : Switch
+{
+}
+#if IOS
+public partial class FreakySwitchHandler : SwitchHandler
+{
+}
+#elif ANDROID
+public partial class FreakySwitchHandler : SwitchHandler
+{
+    protected override AndroidSwitch CreatePlatformView()
+    {
+        return new AndroidSwitch(Context);
+    }
+}
+#endif
