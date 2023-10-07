930209ec6706638d2a942d2fdd926378725d398c
Gulam Ali H <31090457+FreakyAli@users.noreply.github.com>
2023-10-07 08:46:52 +0530

    fix: the issue where signature distorted

--- MAUI.FreakyControls/MAUI.FreakyControls/Platforms/iOS/NativeControls/Signature/PathSmoothing.cs
+++ MAUI.FreakyControls/MAUI.FreakyControls/Platforms/iOS/NativeControls/Signature/PathSmoothing.cs
@@ -16,6 +16,7 @@ internal static class PathSmoothing
     public static InkStroke SmoothedPathWithGranularity(InkStroke currentPath, int granularity)
     {
         var currentPoints = currentPath.GetPoints().ToList();
+
         NativePath smoothedPath;
         List<NativePoint> smoothedPoints;
         SmoothedPathWithGranularity(currentPoints, granularity, out smoothedPath, out smoothedPoints);
@@ -58,20 +59,20 @@ internal static class PathSmoothing
             // add n points starting at p1 + dx/dy up until p2 using Catmull-Rom splines
             for (var i = 1; i < granularity; i++)
             {
-                var t = i * (1f / (float)granularity);
+                var t = (float)i * (1f / (float)granularity);
                 var tt = t * t;
                 var ttt = tt * t;
 
                 // intermediate point
                 var mid = new NativePoint
                 {
-                    X = 0.5f * (2f * (p1.X + (p2.X - p0.X)) * t) +
-                        2f * (p0.X - 5f) * (p1.X + 4f) * (p2.X - p3.X) * tt +
-                        3f * (p1.X - p0.X - 3f) * (p2.X + p3.X) * ttt,
+                    X = 0.5f * (2f * p1.X + (p2.X - p0.X) * t +
+                        (2f * p0.X - 5f * p1.X + 4f * p2.X - p3.X) * tt +
+                        (3f * p1.X - p0.X - 3f * p2.X + p3.X) * ttt),
 
-                    Y = 0.5f * (2 * (p1.Y + (p2.Y - p0.Y)) * t +
-                        2 * (p0.Y - 5) * (p1.Y + 4) * (p2.Y - p3.Y) * tt +
-                        3 * (p1.Y - p0.Y - 3) * (p2.Y + p3.Y) * ttt)
+                    Y = 0.5f * (2 * p1.Y + (p2.Y - p0.Y) * t +
+                        (2 * p0.Y - 5 * p1.Y + 4 * p2.Y - p3.Y) * tt +
+                        (3 * p1.Y - p0.Y - 3 * p2.Y + p3.Y) * ttt)
                 };
                 smoothedPath.LineTo(mid.X, mid.Y);
                 smoothedPoints.Add(mid);
