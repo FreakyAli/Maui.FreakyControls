ddfde637f6e1463dde6a6063561f72ba47e572e9
Ali <194115639+Ali-Tegna@users.noreply.github.com>
2025-04-28 09:29:02 +0530

    fix: memory toolkit test

--- MAUI.FreakyControls/Samples/MauiProgram.cs
+++ MAUI.FreakyControls/Samples/MauiProgram.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Maui;
 using Maui.FreakyControls.Extensions;
+using MemoryToolkit.Maui;
 namespace Samples;
 
 public static class MauiProgram
@@ -16,6 +17,15 @@ public static class MauiProgram
             });
         builder.UseMauiCommunityToolkit();
         builder.InitializeFreakyControls();
+#if DEBUG    
+        // Ensure UseLeakDetection is called after logging has been configured!
+        builder.UseLeakDetection(collectionTarget =>
+        {
+            var MainPage = Application.Current.Windows.FirstOrDefault()?.Page;
+            MainPage?.DisplayAlert("Leak Detected",
+                $"❗🧟❗{collectionTarget.Name} is a zombie!", "OK");
+        });
+#endif
         return builder.Build();
     }
 }
