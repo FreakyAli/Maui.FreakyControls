11206e2c97d4a32f0920f89ccfdc1459868eb2d0
Ali <194115639+Ali-Tegna@users.noreply.github.com>
2026-04-02 14:37:26 +0530

    fix: updated samples for windows and macOS

---
+++ MAUI.FreakyControls/Samples/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+using Foundation;
+
+namespace Samples;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
---
+++ MAUI.FreakyControls/Samples/Platforms/MacCatalyst/Program.cs
@@ -0,0 +1,11 @@
+using UIKit;
+
+namespace Samples;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        UIApplication.Main(args, null, typeof(AppDelegate));
+    }
+}
